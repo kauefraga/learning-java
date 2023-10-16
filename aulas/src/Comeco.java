9b68d68c14dfa6aa7abec46f120c38d7951b39c3
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-10-16 19:14:10 -0300

    :sparkles: feat: complete the first two lessons (variables and data types)

---
+++ aulas/src/Comeco.java
@@ -0,0 +1,12 @@
+public class Comeco {
+    public static void main(String[] args) {
+        System.out.println("Eu amo pizza");
+
+        // Isso é um comentário
+        /*
+        * Isso é
+        * um comentário
+        * maior
+        */
+    }
+}
---
+++ aulas/src/Variaveis.java
@@ -0,0 +1,18 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        int x; // Declaração
+        x = 123; // Atribuição
+        int y = 321; // Inicialização
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+
+        double pi = 3.14;
+        boolean isYourMotherSingle = false;
+        String yourMothersName = "Jossílainëh";
+
+        System.out.println("PI: " + pi);
+        System.out.println("Tua mãe tá solteira? " + isYourMotherSingle);
+        System.out.println("Nome da tua mãe: " + yourMothersName);
+    }
+}
